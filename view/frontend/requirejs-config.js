96e1e8260a54c6189fb072be9b89eefb5a4b67eb
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2026-02-16 10:45:04 +0100

    Lesson: 'Create RequireJS aliases with map and paths'. Created an alias in requirejs-config.js using a 'map', and replaced the long filename with its alias in block-promo-effects.phtml; also added a 'paths' to an external Vue library in requirejs-config.js

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,3 +1,11 @@
 var config = {
-
+    // "map" works only with local AMD modules
+    "map": {
+        "*": {
+            "fadeInElementAlias": "Denal05_MacademyJsFun/js/fade-in-element"
+        }
+    },
+    "paths": {
+        "vue": "https://cdn.jsdelivr.net/npm/vue@2/dist/vue"
+    }
 };
